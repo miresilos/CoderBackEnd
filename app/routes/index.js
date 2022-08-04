5e9875a0e6995c1dbc066b93e5fb7054c0defc31
Miresilos <57138575+miresilos@users.noreply.github.com>
2022-08-03 21:04:51 -0300

    Delete app directory

--- app/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productsRouter = require("./products");
-
-router.use("/productos", productsRouter);
-
-module.exports = router;
