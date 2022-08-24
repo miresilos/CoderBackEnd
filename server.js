5413344bc19466f8b59c3dd5a86ce85f4afe752b
Miresilos <57138575+miresilos@users.noreply.github.com>
2022-08-24 01:14:26 -0300

    Primera base de datos

--- server.js
+++ server.js
@@ -7,20 +7,16 @@ const { Server: IOServer } = require('socket.io');
 const app = express();
 const httpServer = new HttpServer(app);
 const io = new IOServer(httpServer);
+const { mariaDBOptions } = require("./options/mariaDB");
+const { sqliteOptions } = require("./options/sqliteDB");
+const mariaDBKnex = require("knex")(mariaDBOptions);
+const sqliteKnex = require("knex")(sqliteOptions);
+const knexMemory = require("./controller/knexMemory");
 
 app.use(express.static("public"));
 
-const comments = [
-    { username: "juanitoacaiga@gmail.com", text: "El producto llegó en muy buen estado. Recomendado." },
-    { username: "pipes@gmail.com", text: "¿Tienen stock de Esperando a Godot?" },
-    { username: "bateadorbateadorbatea@gmail.com", text: "Apa ¿Qué rompimos?" }
-];
-
-const products = [
-    {title: "Ecce homo", price: "1800", thumbnail: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQmyuNsR7Ps_Fc1wk4AhyyBV7MkKqNs5XlKpT_x-1bsLemtXDqpNHukd51R4ZrUYEIv49g7LLOhetvR&usqp=CAc"},
-    {title: "El extranjero", price: "1600", thumbnail: "https://http2.mlstatic.com/D_NQ_NP_965901-MLA44938805710_022021-V.webp"},
-    {title: "Esperando a Godot", price: "800", thumbnail: "https://caletadelibros.cl/wp-content/uploads/2021/01/22133/portada_esperando-a-godot_samuel-beckett_201801112258.jpg"}
-];
+const comments = [];
+const products = [];
 
 io.on("connection", (socket) => {
     console.log("Un cliente se ha conectado");
@@ -29,11 +25,13 @@ io.on("connection", (socket) => {
 
     socket.on("new-product", (data) => {
         products.push(data);
+        knexMemory.insert(mariaDBKnex, "products", data);
         io.sockets.emit("products", products);
     });
 
     socket.on('new-comment', (data) => {
         comments.push(data);
+        knexMemory.insert(sqliteKnex, "comments", data);
         io.sockets.emit("comments", comments);
     });
 });
@@ -42,4 +40,3 @@ httpServer.listen(PORT, (err) => {
     if (err) console.log("Error in server setup")
     console.log(`Listening on port ${PORT}`);
 });
-
