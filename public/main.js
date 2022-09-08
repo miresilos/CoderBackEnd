520dab45f54b9d420c9f05cd6f044fe23c78ac4e
Miresilos <57138575+miresilos@users.noreply.github.com>
2022-09-07 23:54:55 -0300

    Mocks-y-normalizaci-n

--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
 const socket = io.connect();
 
+socket.on("products", (data) => {
+    renderProducts(data);
+});
+
+
 
 function renderProducts(data) {
     const html = data.map((elem, index) => {   
@@ -7,67 +12,9 @@ function renderProducts(data) {
             `<tr>
                 <td>${elem.title}</td>
                 <td>${elem.price}</td>
-                <td><img src="${elem.thumbnail}" alt="${elem.title}" ></td>
+                <td>${elem.thumbnail}</td>
             </tr>`
         );
     }).join(" ");
     document.getElementById("productsList").innerHTML = html;
 };
-
-function renderComment(data) {
-    const html = data.map((elem, index) => {   
-        return (
-            `<div>
-                <strong>${elem.username}</strong>:
-                <em>${elem.text}</em>
-            </div>`
-        );
-    }).join(" ");
-    document.getElementById("commentsList").innerHTML = html;
-};
-
-function newProduct(e) {
-    let title = document.getElementById('title').value;
-    let price = document.getElementById('price').value;
-    let thumbnail = document.getElementById('thumbnail').value;
-
-    if (title == "" || price == "" || thumbnail == "") {
-        alert("Por favor, rellene el formulario correctamente");
-        return false;
-    } else {
-        const data = {
-            title: title,
-            price: price,
-            thumbnail: thumbnail,
-        };
-    
-        socket.emit('new-product', data);
-        return false;
-    }
-}
-
-function newComment(e) {
-    let username = document.getElementById('username').value;
-    let text = document.getElementById('text').value;
-
-    if(username == "" || text == "") {
-        alert("Por favor, rellene el formulario correctamente");
-        return false;
-    } else {
-        const data = {
-            username: username,
-            text: text,
-        };
-    
-        socket.emit('new-comment', data);
-        return false;
-    }
-};
-
-socket.on("products", (data) => {
-    renderProducts(data);
-});
-
-socket.on("comments", (data) => {
-    renderComment(data);
-});
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-import express from 'express';
-import router from "./src/routes/index.js";
+import express, { json, urlencoded } from 'express';
+const PORT = 8080;
+
+import { Server as HttpServer } from 'http';
+import { Server as IOServer } from 'socket.io';
 
 const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+import router from './src/Routes/routes.js';
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api", router);
+import { dataProd } from "./src/db/dataProd.js";
 
-app.listen(process.env.PORT || 8080, (err) => {
-    if (err) console.log(`Error in server setup: ${err} - ${err.message}`)
-    console.log(`Server listening on Port ${process.env.PORT || 8080}`);
+app.set('view engine', 'ejs');
+app.set('views', 'public');
+// app.use(express.static("public"));
+app.use(json());
+app.use(urlencoded({extended: true}));
+app.use('/api', router)
+
+io.on('connection', socket => {
+  console.log('Cliente conectado', dataProd)
+  io.sockets.emit("products", dataProd);
+
+});
+
+httpServer.listen(PORT, (err) => {
+    if (err) console.log("Error in server setup")
+    console.log(`Listening on port ${PORT}`);
 });
